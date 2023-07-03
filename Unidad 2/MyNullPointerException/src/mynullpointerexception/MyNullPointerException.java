56362803771cc60a9202fd9f8e70f5714cd9f043
David Hernandez Cuevas <davidyepe1212@gmail.com>
2023-07-03 09:56:47 -0600

    se agrega programa MyNullPointerException

---
+++ Unidad 2/MyNullPointerException/src/mynullpointerexception/MyNullPointerException.java
@@ -0,0 +1,15 @@
+package mynullpointerexception;
+
+public class MyNullPointerException {
+
+    public static void main(String[] args) {
+        String text=null;
+        try {
+            int pepe=text.length();
+            } 
+            catch (NullPointerException e) {
+                System.out.println("Hay una NullPointerException ");
+                e.printStackTrace();
+        }
+    }
+}
